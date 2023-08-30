bd83bba164910cc560b5fbffb870cdc59f630232
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-08-29 23:07:37 -0500

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,25 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies';
+import Home from 'pages/HomePage/Home';
+import Movies from 'pages/MoviesPage/Movies';
 import MovieDetails from './MovieDetails/MovieDetails';
 import Cast from './Cast/Cast';
-import Reviews from './MovieRevies/MovieReviews';
+import Reviews from './MovieReviews/MovieReviews';
+import { Layout } from './Layout';
 
 export const App = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/movies">Movies</NavLink>
-          </li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/movies/:id" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />}></Route>
-          <Route path="reviews" element={<Reviews />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="movies" element={<Movies />}></Route>
+          <Route path="movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />}></Route>
+            <Route path="reviews" element={<Reviews />}></Route>
+          </Route>
+          <Route path="*" element={<div>404 Not Found</div>}></Route>
         </Route>
-        <Route path="*" element={<div>404 Not Found</div>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
